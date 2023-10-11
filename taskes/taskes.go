5cf4be5a6505b86547c1bf3ecdf5d5ac0d5dd915
bouhaniHamdi <bouhanihamdi@gmail.com>
2023-10-11 11:32:31 +0200

    add tasks helper function

---
+++ taskes/taskes.go
@@ -0,0 +1,48 @@
+package taskes
+
+import "time"
+
+type Task struct {
+	Job        func() error
+	Duration   time.Duration
+	CreataedAt time.Time
+	LastRun    *time.Time
+}
+
+func NewTask(job func() error, duration time.Duration) *Task {
+	return &Task{
+		Job:        job,
+		Duration:   duration,
+		CreataedAt: time.Now(),
+	}
+}
+
+type Tasks struct {
+	Jobs []*Task
+}
+
+func New() *Tasks {
+	return &Tasks{}
+}
+
+func (t *Tasks) Add(job func() error, duration time.Duration) {
+	t.Jobs = append(t.Jobs, NewTask(job, duration))
+}
+
+func (t *Tasks) Run() error {
+	for _, job := range t.Jobs {
+		j := job
+		go func(job *Task) {
+			ticker := time.NewTicker(job.Duration)
+			for range ticker.C {
+				now := time.Now()
+				j.LastRun = &now
+				err := job.Job()
+				if err != nil {
+					return
+				}
+			}
+		}(j)
+	}
+	return nil
+}
